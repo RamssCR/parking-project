dd084ead4ac56bf3a6b8af0975f23f4e2a00bcad
nose204 <mazomanuel77@gmail.com>
2024-08-31 01:08:49 -0500

    se creo el navbar

--- view/reutils/navbar.php
+++ view/reutils/navbar.php
@@ -1,18 +1,23 @@
-<nav>
-    <ul>
-        <li>
-            <a href="#">Inicio</a>
-        </li>
-        <li>
-            Crear
-            <ul>
-                <li><a href="#">Empleados</a></li>
-                <li><a href="#">Clientes</a></li>
-                <li><a href="#">Vehiculos</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Salir</a>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="../../styles/admin.css" rel="stylesheet">
+    <title>navbar</title>
+</head>
+<body>
+    <!-- Sidebar -->
+    <div id="sidebar" class="p-4">
+        <h2 class="text-xl font-semibold mb-6">Menú</h2>
+        <ul>
+            <li><a href="admin.php" class="block py-2 px-4 rounded hover:bg-gray-700 transition duration-200">Lista de Empleados</a></li>
+            <li><a href="crear_empleado.php" class="block py-2 px-4 rounded hover:bg-gray-700 transition duration-200">Crear Empleado</a></li>
+            
+        </ul>
+    </div>
+</body>
+</html>
