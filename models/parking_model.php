e97568b2922aef8266f7fda4a30b9d83ac49790e
RamssCR <ramsesgpm@gmail.com>
2024-09-03 15:30:41 +0200

    Se conservó la lógica del empleado/usuario únicamente en el objeto

--- models/parking_model.php
+++ models/parking_model.php
@@ -1,53 +1,11 @@
 <?php
 
-class ParkingModel {
+class EmployeeModel {
     private $connection;
 
     public function __construct() {
         $this->connection = mysqli_connect('localhost', 'root', '', 'dbpenta');
     }
-
-    // GET requests
-
-    // Customers
-
-    // Get all customers
-    public function getAll_customers() {
-        return mysqli_execute_query($this->connection, 'SELECT * FROM clientes');
-    }
-
-    // Get one customer
-    public function get_customer($id) : array | bool {
-        $get_customer = mysqli_execute_query($this->connection, 'SELECT * FROM clientes WHERE id_cliente = ?', [$id]);
-        if (!$get_customer) return false;
-
-        return mysqli_fetch_assoc($get_customer);
-    }
-
-    // Vehicles
-
-    // Get all vehicles info
-    public function getAll_vehicles() {
-        return mysqli_execute_query($this->connection, 'SELECT * FROM vehiculos');
-    }
-
-    // Get all customer's vehicles
-    public function getAll_vehicles_customer($id_customer) {
-        $vehicles = mysqli_execute_query($this->connection, 'SELECT * FROM vehiculos WHERE id_cliente = ?', [$id_customer]);
-        if (!$vehicles) return false;
-
-        return $vehicles;
-    }
-
-    // Get one vehicle
-    public function get_vehicle($id) : array | bool {
-        $vehicle = mysqli_execute_query($this->connection, 'SELECT * FROM vehiculos WHERE id_vehiculo = ?', [$id]);
-        if (!$vehicle) return false;
-
-        return mysqli_fetch_assoc($vehicle);
-    }
-
-    // Employees
 
     // Get all employees
     public function getAll_employees() {
