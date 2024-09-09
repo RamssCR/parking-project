674a58e82e1b67f18155fc538d7c64af917480d3
RamssCR <ramsesgpm@gmail.com>
2024-09-09 18:56:35 +0200

    Se creó el CRUD de 'clientes'

--- controllers/customer_controller.php
+++ controllers/customer_controller.php
@@ -20,6 +20,33 @@ class CustomerController {
 
         return $customer;
     }
+
+    // Create a customer
+    public function create_customer($customer) {
+        $insertCustomer = $this->model->create_customer($customer);
+
+        if (!$insertCustomer) return 'Ya existe un cliente con el mismo documento de identidad';
+        if ($insertCustomer == 500) return 'Hubo un error al registrar al cliente';
+        if ($insertCustomer == 201) return 'Cliente registrado exitosamente';
+    }
+
+    // Edit a customer's information
+    public function edit_customer($document, $customer) {
+        $updateCustomer = $this->model->edit_customer($document, $customer);
+
+        if (!$updateCustomer) return 'Este cliente no existe en la base de datos';
+        if ($updateCustomer == 500) return 'Hubo un error al actualizar los datos del cliente';
+        if ($updateCustomer == 200) return 'Cliente modificado exitosamente';
+    }
+
+    // Disable a customer's information
+    public function disable_customer($document) {
+        $disableCustomer = $this->model->disable_customer($document);
+
+        if (!$disableCustomer) return 'El cliente a eliminar no existe';
+        if ($disableCustomer == 500) return 'Hubo un error al eliminar el cliente';
+        if ($disableCustomer == 200) return 'Cliente eliminado exitosamente';
+    }
 }
 
 ?>
