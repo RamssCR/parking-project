a34936a77f0b8074287f11e17c98162fd8eb54e2
RamssCR <ramsesgpm@gmail.com>
2024-09-16 16:10:42 +0200

    Se creó la validación usuario logueado y cerrar sesión

--- models/validators/login_validation.php
+++ models/validators/login_validation.php
@@ -1,9 +1,23 @@
 <?php
     function validateLogin() {
-        if (!isset($_SESSION['user']) && !isset($_SESSION['isAuth']) && !$_SESSION['isAuth']) {
+        if (!isset($_SESSION['user']) && !$_SESSION['isAuth']) {
             unset($_SESSION['user']);
             $_SESSION['isAuth'] = false;
             header('location: ../../index.php');
         }
+    }
+
+    function isLogged() {
+        if (isset($_SESSION['user']) && $_SESSION['isAuth']) {
+            $logged = $_SESSION['user'];
+
+            $logged['tipo_usuario'] == 'Admin' ? header('location: view/admins/admin.php') : header('location: view/empleados/empleado.php');
+        }
+    }
+
+    function logout() {
+        unset($_SESSION['user']);
+        $_SESSION['isAuth'] = false;
+        header('location: ../../index.php');
     }
 ?>
