a44058c2f67de96d3470d20706b10bbfb393213d
RamssCR <ramsesgpm@gmail.com>
2024-09-16 16:10:06 +0200

    correción petición obtener empleado/usuario

--- models/parking_model.php
+++ models/parking_model.php
@@ -17,7 +17,7 @@ class EmployeeModel {
     // Get an employee
     public function get_employee($id) : array | bool {
         $employee = mysqli_execute_query($this->connection, 
-            'SELECT admin.documento, admin.nombre, admin.email, admin.telefono, usuarios.tipo_usuario 
+            'SELECT admin.documento, admin.nombre, admin.email, admin.telefono, usuarios.tipo_usuario, usuarios.pic_user 
              FROM admin INNER JOIN usuarios ON admin.email = usuarios.email WHERE documento = ? UNION 
              SELECT empleados.documento, empleados.nombre, empleados.email, empleados.telefono, usuarios.tipo_usuario, usuarios.pic_user
              FROM empleados INNER JOIN usuarios ON empleados.email = usuarios.email WHERE documento = ?',
