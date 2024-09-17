35e6867f07ea7ed984cf2b350841e91d9ab91b57
RamssCR <ramsesgpm@gmail.com>
2024-09-17 20:18:53 +0200

    Se creó el MC para pago

---
+++ controllers/payment_controller.php
@@ -0,0 +1,51 @@
+<?php
+require_once('../../models/payment_model.php');
+
+class PaymentController{
+    private $model;
+
+    public function __construct() {
+       $this->model = new PaymentModel(); 
+    }
+
+    // Fetch all payments of a single vehicle
+    public function show_vehicle_payment($id_vehicle) {
+        $payments = $this->model->get_vehicle_payments($id_vehicle);
+
+        if (!$payments) return 'No hay pagos registrados';
+        return $payments;
+    }
+
+    // Fetch a payment
+    public function show_payment($id) {
+        $payment = $this->model->get_payment($id);
+
+        if (!$payment) return 'No existe dicho pago';
+        return $payment;
+    }
+
+    // Create a payment
+    public function create_payment($payment) {
+        $insertPayment = $this->model->create_payment($payment);
+        return !$insertPayment ? 'Hubo un error al crear el pago' : 'Pago creado exitosamente';
+    }
+
+    // Modify a payment
+    public function edit_payment($id, $payment) {
+        $editPayment = $this->model->edit_payment($id, $payment);
+
+        if (!$editPayment) return 'No existe dicho pago';
+        if ($editPayment == 500) return 'Hubo un error al modificar los datos del pago';
+        if ($editPayment == 200) return 'Pago modificado exitosamente';
+    }
+
+    // Disable a payment
+    public function disable_payment($id) {
+        $disablePayment = $this->model->disable_payment($id);
+
+        if (!$disablePayment) return 'No existe dicho pago';
+        if ($disablePayment == 500) return 'Hubo un error al eliminar el pago';
+        if ($disablePayment == 200) return 'Pago eliminado exitosamente';
+    }
+}
+?>
---
+++ models/payment_model.php
@@ -0,0 +1,51 @@
+<?php
+class PaymentModel{
+    private $connection;
+
+    public function __construct() {
+        $this->connection = mysqli_connect('localhost', 'root', '', 'dbpenta');
+    }
+
+    // Fetch all payments of a single vehicle
+    public function get_vehicle_payments($id_vehicle) {
+        $payments = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_vehiculo = ? AND deshabilitado = ?', [$id_vehicle, 0]);
+        return $payments ? $payments : false;
+    }
+
+    // Fetch a payment
+    public function get_payment($id) {
+        $payment = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
+        return $payment ? $payment : false;
+    }
+
+    // Create a payment
+    public function create_payment($payment) {
+        $insertPayment = mysqli_execute_query($this->connection, 'INSERT INTO pago (total, tiempo, tarifa, id_vehiculo) VALUES (?, ?, ?, ?)',
+            [$payment['total'], $payment['time'], $payment['base_price'], $payment['id_vehicle']]
+        );
+
+        if (!$insertPayment) return false;
+        return true;
+    }
+
+    // Modify a payment
+    public function edit_payment($id, $payment) : int | bool {
+        $isExisting = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
+        if (!$isExisting) return false;
+
+        $editPayment = mysqli_execute_query($this->connection, 'UPDATE pago SET tiempo = ?, total = ? WHERE id_pago = ?', [$payment['time'], $payment['total'], $id]);
+        if (!$editPayment) return 500;
+        return 200;
+    }
+
+    // Disable a payment
+    public function disable_payment($id) {
+        $isExisting = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
+        if (!$isExisting) return false;
+
+        $disablePayment = mysqli_execute_query($this->connection, 'UPDATE pago SET deshabilitado = ? WHERE id_pago = ?', [1, $id]);
+        if (!$disablePayment) return 500;
+        return 200;
+    }
+}
+?>
