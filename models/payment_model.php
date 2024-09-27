0fa534db8fd93ff733a3cbb86afebd4a7c863fbb
RamssCR <ramsesgpm@gmail.com>
2024-09-27 18:44:43 +0200

    se corrigieron validaciones de datos

--- models/payment_model.php
+++ models/payment_model.php
@@ -9,13 +9,13 @@ class PaymentModel{
     // Fetch all payments of a single vehicle
     public function get_vehicle_payments($id_vehicle) {
         $payments = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_vehiculo = ? AND deshabilitado = ?', [$id_vehicle, 0]);
-        return $payments ? $payments : false;
+        return mysqli_num_rows($payments) >= 1 ? $payments : false;
     }
 
     // Fetch a payment
     public function get_payment($id) {
         $payment = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
-        return $payment ? $payment : false;
+        return mysqli_num_rows($payment) >= 1 ? $payment : false;
     }
 
     // Create a payment
@@ -31,7 +31,7 @@ class PaymentModel{
     // Modify a payment
     public function edit_payment($id, $payment) : int | bool {
         $isExisting = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
-        if (!$isExisting) return false;
+        if (mysqli_num_rows($isExisting) == 0) return false;
 
         $editPayment = mysqli_execute_query($this->connection, 'UPDATE pago SET tiempo = ?, total = ? WHERE id_pago = ?', [$payment['time'], $payment['total'], $id]);
         if (!$editPayment) return 500;
@@ -41,7 +41,7 @@ class PaymentModel{
     // Disable a payment
     public function disable_payment($id) {
         $isExisting = mysqli_execute_query($this->connection, 'SELECT * FROM pago WHERE id_pago = ? AND deshabilitado = ?', [$id, 0]);
-        if (!$isExisting) return false;
+        if (mysqli_num_rows($isExisting) == 0) return false;
 
         $disablePayment = mysqli_execute_query($this->connection, 'UPDATE pago SET deshabilitado = ? WHERE id_pago = ?', [1, $id]);
         if (!$disablePayment) return 500;
